30e31e6c7b288d1abfb5b4df6fd643f57510531a
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-06-11 00:11:14 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,21 @@
 // Bomb Component Code Goes Here
+// Bomb Component Code Goes Here
+import React from 'react';
+
+class Bomb extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render() {
+    <div className="ImageSlider">
+      I am on slide {this.slide.currentSlideIndex}
+    </div>
+  }
+}
+
+export default Bomb;
